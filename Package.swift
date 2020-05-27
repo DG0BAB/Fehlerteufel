1d5f35597b2270a1e9693609b0294f5553d475f8
Joachim Deelen <jdeelen@micabo.de>
2020-05-27 14:02:21 +0200

    Initial import of package description

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// Name of this Package
+let packageName = "Fehlerteufel"
+
+// Package creation
+let package = Package(name: packageName)
+
+// Products define the executables and libraries produced by a package, and make them visible to other packages.
+package.products = [.library(name: packageName, targets: [packageName])]
+package.platforms = [.iOS(.v13), .macOS(.v10_15)]
+package.dependencies = [.package(url: "git@github.com:DG0BAB/Clause.git", .branch("master"))]
+package.targets = [
+	.target(name: packageName, dependencies: ["Clause"], path: "Sources"),
+	.testTarget(name: "\(packageName)Tests", dependencies: [Target.Dependency(stringLiteral: packageName)], path: "Tests"),
+]
