7c000fc655750ca48865f1960ecf00026bcd6cbc
Joachim Deelen <jdeelen@micabo.de>
2020-05-29 15:31:40 +0200

    Switched top local dependencies

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(name: packageName)
 // Products define the executables and libraries produced by a package, and make them visible to other packages.
 package.products = [.library(name: packageName, targets: [packageName])]
 package.platforms = [.iOS(.v13), .macOS(.v10_15)]
-package.dependencies = [.package(url: "git@github.com:DG0BAB/Clause.git", .branch("develop"))]
+package.dependencies = [
+//	.package(url: "git@github.com:DG0BAB/Clause.git", .branch("develop")),
+	.package(name: "Clause", path: "../../Clause/ClausePackage"),
+]
 package.targets = [
 	.target(name: packageName, dependencies: ["Clause"], path: "Sources"),
 	.testTarget(name: "\(packageName)Tests", dependencies: [Target.Dependency(stringLiteral: packageName)], path: "Tests"),
