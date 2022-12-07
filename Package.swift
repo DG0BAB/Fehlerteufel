ed3fbc0d39f92326ef240bb625473eaeec71bad4
Joachim Deelen <jdeelen@micabo.de>
2022-12-07 14:21:49 +0100

    Changed dependencies from SSH to HTTPS

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ package.defaultLocalization = "en"
 package.products = [.library(name: packageName, targets: [packageName])]
 package.platforms = [.iOS(.v13), .macOS(.v10_15)]
 package.dependencies = [
-	.package(url: "git@github.com:DG0BAB/Clause.git", .branch("master")),
+	.package(url: "https://github.com/DG0BAB/Clause.git", .branch("master")),
 ]
 package.targets = [
 	.target(name: packageName, dependencies: ["Clause"], path: "Sources", resources: [Resource.process("Resources")]),
